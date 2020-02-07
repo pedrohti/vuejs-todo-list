c46d126683f586d7ad07855335762085d43be45b
PedroHMoraes <pedroh.ti@hotmail.com>
2020-02-07 19:31:14 -0300

    Lista de tarefas feita com VueJS

---
+++ src/components/FormAddTask.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="form-todo form-group">
+		<h1>Lista de Tarefas</h1>
+		<p>
+			<input placeholder="Adicione uma tarefa" type="text" name="task" class="form-control" v-model="name">
+		</p>
+		<button type="submit" v-on:click="addTask" class="btn btn-primary">Adicionar</button>
+		<hr />
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			name: '',
+			done: ''
+		}
+	},
+	methods: {
+		addTask() {
+			if(this.name.trim() === '') { return }
+
+			this.$emit('add-task', {
+				name: this.name,
+				done: false
+			})
+
+			this.name = ''
+		},
+	},
+}
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,71 @@
+<template>
+	<div class="container">
+		<FormAddTask v-on:add-task="addTask"/>
+		<div v-for="(t, i) in tasks" v-bind:key="i" class="row" :class="{removido: t.done}">
+			<div class="col col-11 label" v-on:click="checkDone(t)">
+				{{t.name}}
+			</div>
+			<div class="col trash">
+				<em href="#" v-on:click.prevent="removeTask(i)" title="Excluir" class="fas fa-trash" />
+			</div>
+		</div>
+		<!-- <ul v-for="(t, i) in tasks" v-bind:key="i" class="list-group">
+			<li :class="{removido: t.done}" class="list-group-item d-flex justify-content-between align-items-center" v-on:click="checkDone(t)">{{t.name}}<i href="#" v-on:click.prevent="removeTask(i)" title="Excluir" class="fa fa-trash"></i></li>
+		</ul> -->
+	</div>
+</template>
+
+<script>
+import FormAddTask from './FormAddTask'
+
+
+export default {
+	components: {
+		FormAddTask
+	},
+	data() {
+		return {
+			tasks: []
+		}
+	},
+	methods: {
+		addTask(t) {
+			this.tasks.push(t)
+		},
+		removeTask(i) {
+			this.tasks.splice(i, 1)
+		},
+		checkDone(t) {
+			t.done = !t.done
+		}
+	}
+}
+</script>
+
+<style>
+	ul li {
+		list-style-type: none;
+	}
+
+	.removido {
+		color: gray;
+		text-decoration: line-through;
+		background-color: rgb(0,128,0, 0.1);
+	}
+
+	em {
+		color: red;
+	}
+
+	/* .col {
+		border: 1px red solid;
+	} */
+
+	.label {
+		text-align: left;
+	}
+
+	.trash {
+		text-align: right;
+	}
+</style>
